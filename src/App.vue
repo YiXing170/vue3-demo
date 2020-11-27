fbe96e29bc0a1a8826cc92bef568cc1a76be3c60
yijiliang <857719943@qq.com>
2020-11-27 22:41:35 +0800

    setup生命周期使用

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo"
+       src="./assets/logo.png" />
+  <div>
+    <h2>欢迎光临红浪漫洗浴中心</h2>
+    <div>请选择一位美女为你服务</div>
+  </div>
+  <div>
+    <button @click="selectGirlFun(index)"
+            v-for="(item, index) in girls"
+            :key="index">
+      {{ index }} : {{ item }}
+    </button>
+  </div>
+  <div>你选择了【{{ selectGirl }}】为你服务</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import {
+  defineComponent,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  reactive,
+  toRefs,
+} from "vue";
+interface DataProps {
+  girls: string[];
+  selectGirl: string;
+  selectGirlFun: (index: number) => void;
+}
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: {},
+  setup() {
+    console.log("1-开始创建组件-----setup()");
+    // const girls = ref(["1号", "2号", "3号"]);
+    // const selectGirl = ref("");
+    // const selectGirlFun = (index: number) => {
+    //   selectGirl.value = girls.value[index];
+    // };
+    onBeforeMount(() => {
+      console.log("2-组件挂载到页面之前执行-----onBeforeMount()");
+    });
+
+    onMounted(() => {
+      console.log("3-组件挂载到页面之后执行-----onMounted()");
+    });
+    onBeforeUpdate(() => {
+      console.log("4-组件更新之前-----onBeforeUpdate()");
+    });
+    onUpdated(() => {
+      console.log("5-组件更新之后-----onUpdated()");
+    });
+    const data: DataProps = reactive({
+      girls: ["1号", "2号", "3号"],
+      selectGirl: "",
+      selectGirlFun: (index: number) => {
+        data.selectGirl = data.girls[index];
+      },
+    });
+    const reactData = toRefs(data);
+    return {
+      ...reactData,
+    };
+  },
 });
 </script>
 
